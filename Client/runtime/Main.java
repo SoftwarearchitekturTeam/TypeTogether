1d8033abd83bb3d652251d20c167b06d2985c1d1
Sparkobbable <j.wilmesmeier@gmail.com>
2021-09-15 14:03:37 +0200

    init

---
+++ Client/runtime/Main.java
@@ -0,0 +1,7 @@
+package Client.runtime;
+
+public class Main {
+    public static void main(String[] args) {
+        
+    }
+}
---
+++ Server/runtime/Main.java
@@ -0,0 +1,7 @@
+package Server.runtime;
+
+public class Main {
+    public static void main(String[] args) {
+        
+    }
+}
