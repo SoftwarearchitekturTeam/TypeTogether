6c550c82d5da0c7313359fe6093225bfd1aed6bd
Jan Philip <Frehe>
2021-09-21 19:00:54 +0200

    Baseline to create testcases for the documentchar

---
+++ networking/src/test/java/de/hswhameln/typetogether/networking/types/DocumentCharacterTest.java
@@ -0,0 +1,11 @@
+package de.hswhameln.typetogether.networking.types;
+
+import org.junit.jupiter.api.Test;
+
+public class DocumentCharacterTest {
+   
+    @Test
+    public void doTest() {
+        System.out.println("Test");
+    }
+}
