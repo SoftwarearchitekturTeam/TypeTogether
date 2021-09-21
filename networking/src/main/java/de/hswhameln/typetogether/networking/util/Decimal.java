3cf020f2898c97d58e7c85b8ceb6190550fde40b
Jan Philip <Frehe>
2021-09-21 18:46:03 +0200

    Datastructure Index of char and Start of insertion algorithm

---
+++ networking/src/main/java/de/hswhameln/typetogether/networking/util/Decimal.java
@@ -0,0 +1,63 @@
+package de.hswhameln.typetogether.networking.util;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Optional;
+import java.util.stream.Collectors;
+
+import de.hswhameln.typetogether.networking.types.Identifier;
+
+public class Decimal {
+    
+    public static List<Integer> fromIdentifierList(List<Identifier> identifiers) {
+        return identifiers.stream().map(ident -> ident.getDigit()).collect(Collectors.toList());
+    }
+
+    public static List<Integer> increment(List<Integer> n1, List<Integer> delta) {
+        int firstNonzeroDigit = -1;
+        List<Integer> inc = new ArrayList<>();
+        for(int i = 0; i < delta.size(); i++) {
+            if(delta.get(i) != 0 && firstNonzeroDigit != -1) {
+                firstNonzeroDigit = i;
+            }
+        }
+        inc = delta.subList(0, firstNonzeroDigit);
+        inc.add(0);
+        inc.add(1);
+
+        List<Integer> v1 = new ArrayList<>();
+        for(int i = 0; i < n1.size(); i++) {
+            v1.set(i, n1.get(i));
+        }
+        for(Integer i : inc) {
+            v1.add(i);
+        }
+
+        List<Integer> v2 = new ArrayList<>();
+        if(v1.get(v1.size() -1 ) == 0) {
+            for(Integer i : inc) {
+                v2.add(i);
+            }
+        } else {
+            v2 = v1;
+        }
+        return v2;
+    }
+
+    public static List<Identifier> toIdentifierList(List<Integer> n, List<Identifier> before, List<Identifier> after, int userId) {
+        List<Identifier> returnIn = new ArrayList<>();
+        for(int i = 0; i < n.size(); i++) {
+            if(i == n.size() - 1) {
+                returnIn.add(new Identifier(n.get(i), userId));
+            } else if(i < before.size() && n.get(i) == before.get(i).getDigit()) {
+                returnIn.add(new Identifier(n.get(i), before.get(i).getUserId()));
+            } else if(i < after.size() && n.get(i) == after.get(i).getDigit()) {
+                returnIn.add(new Identifier(n.get(i), after.get(i).getUserId()));
+            } else {
+                returnIn.add(new Identifier(n.get(i), userId));
+            }
+        }
+        return returnIn;
+    }
+}
