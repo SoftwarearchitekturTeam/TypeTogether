9342b94fa55dcb8147b91d561160fda435b10047
Jan Philip <Frehe>
2021-09-22 18:56:53 +0200

    Insertion algorithm usecase3 working

--- networking/src/main/java/de/hswhameln/typetogether/networking/util/Decimal.java
+++ networking/src/main/java/de/hswhameln/typetogether/networking/util/Decimal.java
@@ -28,34 +28,20 @@ public class Decimal {
     }
 
     public static List<Integer> increment(List<Integer> n1, List<Integer> delta) {
-        int firstNonzeroDigit = delta.indexOf(0) - 1;
-        if(firstNonzeroDigit < 0) {
-            firstNonzeroDigit = delta.size();
+        int firstNonzeroDigit = 0;
+        for(int i = 0; i < delta.size() && delta.get(i) > 0; i++) {
+            firstNonzeroDigit = i;
         }
+
         List<Integer> inc = new ArrayList<>();
         inc = delta.subList(0, firstNonzeroDigit);
         
         //TODO critical lines. Needed for Usecase2 but not Usecase 1
         inc.add(0);
-        inc.add(1);
+        inc.add(1); //TODO position for possible addition of programm wide counter
 
-        List<Integer> v1 = new ArrayList<>();
-        for(int i = 0; i < n1.size(); i++) {
-            v1.add(n1.get(i));
-        }
-        for(Integer i : inc) {
-            v1.add(i);
-        }
-
-        List<Integer> v2 = new ArrayList<>();
-        if(v1.get(v1.size() -1 ) == 0) {
-            for(Integer i : inc) {
-                v2.add(i);
-            }
-        } else {
-            v2 = v1;
-        }
-        return v2;
+        List<Integer> v1 = Decimal.add(n1, inc);
+        return v1.get(v1.size() - 1) == 0 ? Decimal.add(v1, inc) : v1;
     }
 
     public static List<Identifier> toIdentifierList(List<Integer> n, List<Identifier> before, List<Identifier> after, int userId) {
@@ -63,8 +49,10 @@ public class Decimal {
         for(int i = 0; i < n.size(); i++) {
             if(i == n.size() - 1) {
                 returnIn.add(new Identifier(n.get(i), userId));
+
             } else if(i < before.size() && n.get(i) == before.get(i).getDigit()) {
                 returnIn.add(new Identifier(n.get(i), before.get(i).getUserId()));
+
             } else if(i < after.size() && n.get(i) == after.get(i).getDigit()) {
                 returnIn.add(new Identifier(n.get(i), after.get(i).getUserId()));
             } else {
@@ -74,15 +62,47 @@ public class Decimal {
         return returnIn;
     }
 
-    public static List<Integer> subtractGreaterThan(List<Integer> n1, List<Integer> n2) {
+    public static List<Integer> add(List<Integer> n1, List<Integer> n2) {
         List<Integer> returnIn = new ArrayList<>();
         for(int i = 0; i < Math.max(n1.size(), n2.size()); i++) {
+            if(i >= n1.size()) {
+                returnIn.add(n2.get(i));
+            } else if(i >= n2.size()) {
+                returnIn.add(n1.get(i));
+            } else {
+                returnIn.add(n1.get(i) + n2.get(i));
+            }
+        }
+        return returnIn;
+    }
+
+    public static List<Integer> subtractGreaterThan(List<Integer> n1, List<Integer> n2) {
+        List<Integer> returnIn = new ArrayList<>();
+
+        int maxSize = Math.max(n1.size(), n2.size());
+        int difference = 0;
+        boolean n2IsBigger = false;
+        if(n1.size() < maxSize) {
+            difference = maxSize - n1.size();
+            n2IsBigger = true;
+        } else if(n2.size() < maxSize) {
+            difference = maxSize - n2.size();
+        }
+
+        for(int i = 0; i < (maxSize - difference); i++) {
             if(n1.get(i) == n2.get(i)) {
                 returnIn.add(0);
             } else if(n1.get(i) > n2.get(i)) {
                 returnIn.add(n1.get(i) - n2.get(i));
             } else {
                 returnIn.add(n2.get(i) - n1.get(i));
+            }
+        }
+        for(int i = 1; i < difference; i++) {
+            if(n2IsBigger) {
+                returnIn.add(n2.get(n1.size() + difference));
+            } else {
+                returnIn.add(n1.get(n2.size() + difference));
             }
         }
         return returnIn;
