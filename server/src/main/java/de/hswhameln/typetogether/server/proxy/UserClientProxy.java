a5fcdf85d383db208516716f2991cd1b43fddd54
Lars Kecker <lars.kecker2009@gmail.com>
2021-09-22 23:01:57 +0200

    implemented UserClientProxy

--- server/src/main/java/de/hswhameln/typetogether/server/proxy/UserClientProxy.java
+++ server/src/main/java/de/hswhameln/typetogether/server/proxy/UserClientProxy.java
@@ -2,67 +2,52 @@ package de.hswhameln.typetogether.server.proxy;
 
 import de.hswhameln.typetogether.networking.api.Document;
 import de.hswhameln.typetogether.networking.api.User;
+import de.hswhameln.typetogether.networking.proxy.ObjectResolver;
 import de.hswhameln.typetogether.networking.shared.AbstractClientProxy;
 import de.hswhameln.typetogether.networking.shared.DocumentClientProxy;
+import de.hswhameln.typetogether.networking.shared.helperinterfaces.Action;
+import de.hswhameln.typetogether.networking.shared.helperinterfaces.UnsafeSupplier;
+import de.hswhameln.typetogether.networking.util.IOUtils;
 
+import java.io.IOException;
 import java.net.Socket;
-import java.util.HashMap;
-import java.util.Map;
-import java.util.logging.Level;
 
 public class UserClientProxy extends AbstractClientProxy implements User {
 
-    Map<Integer, Document> localDocumentsByCommunicationIds = new HashMap<>();
+    private final ObjectResolver<Document> localDocumentResolver;
 
     public UserClientProxy(Socket socket) {
         super(socket);
-    }
-
-    private Document resolveDocument() {
-        try {
-            String sCommunicationId = this.in.readLine();
-            int communicationId = Integer.parseInt(sCommunicationId);
-            this.logger.info("Resolving document for Com-ID: " + communicationId);
-    
-            if (!this.localDocumentsByCommunicationIds.containsKey(communicationId)) {
-    
-                this.out.println("1");
-    
-                String sPort = this.in.readLine();
-    
-                int port = Integer.parseInt(sPort);
-                DocumentClientProxy clientProxy = new DocumentClientProxy(new Socket(this.socket.getInetAddress(), port));
-    
-                this.localDocumentsByCommunicationIds.put(communicationId, clientProxy);
-            } else {
-                this.logger.warning("Could not resolve document for Com-ID: " + communicationId + " already in use");
-                this.out.println("0");
-            }
-            return this.localDocumentsByCommunicationIds.get(communicationId);
-        } catch (Exception e) {
-            this.out.println("[ERROR]: Could not read input: " + e.getMessage());
-            this.logger.log(Level.SEVERE, "Could not read input", e);
-            return null;
-        }
+        this.localDocumentResolver = new ObjectResolver<>(DocumentClientProxy::new, this.in, this.out, this.socket.getInetAddress());
     }
 
     @Override
     public int getId() {
-        // TODO Auto-generated method stub
-        return 0;
+        return safelyExecute(() -> {
+            this.chooseOption("1");
+            IOUtils.expectResponseCodeSuccess(this.in);
+            int id = Integer.parseInt(this.in.readLine());
+            logger.fine("getId returned " + id);
+            return id;
+        });
     }
 
     @Override
     public String getName() {
-        // TODO Auto-generated method stub
-        return null;
+        return safelyExecute(() -> {
+            this.chooseOption("2");
+            String name = this.in.readLine();
+            logger.fine("getName returned " + name);
+            return name;
+        });
     }
 
     @Override
     public Document getDocument() {
-        // TODO Auto-generated method stub
-        return null;
+        return safelyExecute(() -> {
+            this.chooseOption("3");
+            return this.localDocumentResolver.resolveObject();
+        });
     }
 
-    // TODO
 }
