a9798cb3d0a7b808148ae4be061676d3fe04dff4
Lars Kecker <lars.kecker2009@gmail.com>
2021-09-24 10:11:16 +0200

    added dummy ServerSocket creation to consoleClient

--- console-client/src/main/java/de/hswhameln/typetogether/testclient/ConsoleClient.java
+++ console-client/src/main/java/de/hswhameln/typetogether/testclient/ConsoleClient.java
@@ -1,6 +1,8 @@
 package de.hswhameln.typetogether.testclient;
 
+import java.awt.*;
 import java.io.*;
+import java.net.ServerSocket;
 import java.net.Socket;
 import java.util.Scanner;
 
@@ -23,12 +25,19 @@ public class ConsoleClient {
     public void start() throws IOException {
         boolean running = true;
         System.out.println("Press any input to send to the server or /quit to stop.");
+        System.out.println("or /open <port> to start a client-side server.");
         while (running && socket.isConnected()) {
             String input = sc.nextLine();
             if ("".equals(input)) {
                 System.out.println("Server sent: " + in.readLine());
             } else if ("/quit".equals(input)) {
                 running = false;
+            } else if (input.startsWith("/open")) {
+                int port = Integer.parseInt(input.split(" ")[1]);
+                new Thread(() -> {
+                    startDummyServer(port);
+                }).start();
+
             } else {
                 out.println(input);
                 System.out.println("Server replied: " + in.readLine());
@@ -36,6 +45,18 @@ public class ConsoleClient {
         }
 
         this.cleanUp();
+    }
+
+    private void startDummyServer(int port) {
+        try {
+            ServerSocket serverSocket = new ServerSocket(port);
+            Socket clientSocket = serverSocket.accept();
+            PrintWriter out = new PrintWriter(new OutputStreamWriter(clientSocket.getOutputStream()), true);
+            out.println("0"); // 0 commands available
+            System.out.println("Socket connection established with client " + clientSocket.getInetAddress().getHostAddress() + ":" + clientSocket.getLocalPort());
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 
     public void cleanUp() throws IOException {
