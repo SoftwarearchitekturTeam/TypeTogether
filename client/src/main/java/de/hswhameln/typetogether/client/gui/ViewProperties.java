4a362afd72fcd2e216be5bb90bad76d4d3ee7c10
Jan Philip <Frehe>
2021-09-28 15:24:11 +0200

    Fixed Grid for MenuPanel going towards final look

--- client/src/main/java/de/hswhameln/typetogether/client/gui/ViewProperties.java
+++ client/src/main/java/de/hswhameln/typetogether/client/gui/ViewProperties.java
@@ -10,7 +10,7 @@ public interface ViewProperties {
 	public static final Color BACKGROUND_COLOR = new Color(255, 255, 255);
 	public static final Color FONT_COLOR = new Color(52, 73, 94);
 	public static final Color CONTRAST_COLOR = new Color(154, 198, 197);
-	public static final Font SUBHEADLINE_FONT = new Font("Arial", Font.PLAIN, 9);
+	public static final Font SUBHEADLINE_FONT = new Font("Arial", Font.PLAIN, 18);
 	public static final Font HEADLINE_FONT = new Font("Calibri", Font.BOLD, 40);
 	public static final float BUTTON_FONT_SIZE = 16f;
 	public static final float MENU_BUTTON_FONT_SIZE = 12f;
