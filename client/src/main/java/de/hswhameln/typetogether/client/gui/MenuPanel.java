f4066f707aff66ecae4e3b1ff512cd3cc63896ca
Jan Philip <Frehe>
2021-09-28 11:28:55 +0200

    Create Grid for MenuPanel

--- client/src/main/java/de/hswhameln/typetogether/client/gui/MenuPanel.java
+++ client/src/main/java/de/hswhameln/typetogether/client/gui/MenuPanel.java
@@ -5,10 +5,32 @@ import javax.swing.JPanel;
 
 public class MenuPanel extends JPanel {
     
-    private JPanel headlin;
+    private JPanel headline;
+    private JPanel leftSide;
+    private JPanel rightSide;
 
     public MenuPanel() {
         this.setLayout(new BoxLayout(this, BoxLayout.PAGE_AXIS));
+        this.setSize(ViewProperties.DEFAULT_WIDTH, ViewProperties.DEFAULT_HEIGHT);
+        this.createGrid();
+        this.rightSide.setBackground(ViewProperties.BACKGROUND_COLOR);
         
+
+    }
+
+    private void createGrid() {
+        this.headline = new TypeTogetherPanel();
+        this.add(this.headline);
+        JPanel body = new JPanel();
+        body.setLayout(new java.awt.FlowLayout());
+        this.add(body);
+        this.leftSide = new JPanel();
+        body.add(this.leftSide);
+        this.rightSide = new JPanel();
+        body.add(this.rightSide);
+    }
+
+    private void createLeftSide() {
+
     }
 }
