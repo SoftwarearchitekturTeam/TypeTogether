c9714d68aa7cb9c7b35d60e211c3b2ab82ab2385
Sparkobbable <j.wilmesmeier@gmail.com>
2021-09-28 12:30:03 +0200

    First editor try

--- client/src/main/java/de/hswhameln/typetogether/client/gui/TypeTogetherPanel.java
+++ client/src/main/java/de/hswhameln/typetogether/client/gui/TypeTogetherPanel.java
@@ -1,5 +1,8 @@
 package de.hswhameln.typetogether.client.gui;
 
+import java.awt.Dimension;
+
+import javax.swing.BorderFactory;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
 
@@ -9,6 +12,10 @@ public class TypeTogetherPanel extends JPanel {
     
     public TypeTogetherPanel() {
         this.headline = new JLabel(ViewProperties.HEADLINE);
+        this.headline.setFont(ViewProperties.HEADLINE_FONT);
         this.setBackground(ViewProperties.CONTRAST_COLOR);
+        this.setMaximumSize(new Dimension(ViewProperties.DEFAULT_WIDTH * 2, 100));
+        this.add(headline);
+        this.setVisible(true);
     }
 }
