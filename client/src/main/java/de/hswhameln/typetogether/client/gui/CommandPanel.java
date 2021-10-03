9125d53850c2209697b323c856d22b337602c808
Jan Philip <Frehe>
2021-10-03 18:08:35 +0200

    Add undo and redo buttons

--- client/src/main/java/de/hswhameln/typetogether/client/gui/CommandPanel.java
+++ client/src/main/java/de/hswhameln/typetogether/client/gui/CommandPanel.java
@@ -1,7 +1,6 @@
 package de.hswhameln.typetogether.client.gui;
 
-import javax.swing.JButton;
-import javax.swing.JPanel;
+import javax.swing.*;
 
 import java.awt.BorderLayout;
 import java.awt.Dimension;
@@ -14,6 +13,7 @@ public class CommandPanel extends AbstractPanel {
 
     private JButton undo;
     private JButton redo;
+    private JMenuBar menubar;
     private JPanel editorPanel;
 
    public CommandPanel (MainWindow window, SessionStorage sessionStorage) {
@@ -24,13 +24,20 @@ public class CommandPanel extends AbstractPanel {
         this.setPreferredSize(new Dimension(ViewProperties.DEFAULT_WIDTH, ViewProperties.DEFAULT_HEIGHT));
         this.setVisible(true);
         this.setBackground(Color.WHITE);
-        undo = new JButton();
-        redo = new JButton();
+
+        this.menubar = new JMenuBar();
+        undo = new JButton("Undo");
+        undo.setForeground(ViewProperties.FONT_COLOR);
+        undo.setVisible(true);
+        redo = new JButton("Redo");
+        redo.setForeground(ViewProperties.FONT_COLOR);
+        redo.setVisible(true);
         editorPanel = new EditorPanel(window, sessionStorage);
         undo.addActionListener(a-> this.undo());
         redo.addActionListener(a-> this.redo());
-        this.add(undo, BorderLayout.NORTH);
-        this.add(redo, BorderLayout.NORTH);
+        this.menubar.add(undo);
+        this.menubar.add(redo);
+        this.add(this.menubar, BorderLayout.NORTH);
         this.add(editorPanel, BorderLayout.CENTER);
 
    }
