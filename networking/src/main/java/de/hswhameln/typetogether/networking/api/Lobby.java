b7a70a921b9174bdab6add70714485df2125c824
Lars Kecker <lars.kecker2009@gmail.com>
2021-10-04 15:30:26 +0200

    implemented getDocuments in lobby

--- networking/src/main/java/de/hswhameln/typetogether/networking/api/Lobby.java
+++ networking/src/main/java/de/hswhameln/typetogether/networking/api/Lobby.java
@@ -3,6 +3,8 @@ package de.hswhameln.typetogether.networking.api;
 import de.hswhameln.typetogether.networking.api.exceptions.InvalidDocumentIdException;
 import de.hswhameln.typetogether.networking.api.exceptions.UnknownUserException;
 
+import java.util.Collection;
+
 public interface Lobby {
 
     void joinDocument(User user, String documentId) throws InvalidDocumentIdException.DocumentDoesNotExistException;
@@ -14,4 +16,7 @@ public interface Lobby {
     void createDocument(String documentId) throws InvalidDocumentIdException.DocumentAlreadyExistsException;
 
     void deleteDocument(User user, String documentId) throws InvalidDocumentIdException.DocumentDoesNotExistException;
+
+    Collection<Document> getDocuments();
+
 }
