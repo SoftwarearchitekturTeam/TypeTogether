d4d0720e54490c83c8f9b45a2db1376436738df3
Lars Kecker <lars.kecker2009@gmail.com>
2021-10-04 15:38:28 +0200

    added default addChars and removeChars method

--- networking/src/main/java/de/hswhameln/typetogether/networking/api/Document.java
+++ networking/src/main/java/de/hswhameln/typetogether/networking/api/Document.java
@@ -2,9 +2,19 @@ package de.hswhameln.typetogether.networking.api;
 
 import de.hswhameln.typetogether.networking.types.DocumentCharacter;
 
+import java.util.Collection;
+
 public interface Document {
     void addChar(User author, DocumentCharacter character);
     void removeChar(User author, DocumentCharacter character);
     void close(User source);
     String getFuncId();
+
+    default void addChars(User author, Collection<DocumentCharacter> characters) {
+        characters.forEach(c -> this.addChar(author, c));
+    }
+
+    default void removeChars(User author, Collection<DocumentCharacter> characters) {
+        characters.forEach(c -> this.removeChar(author, c));
+    }
 }
