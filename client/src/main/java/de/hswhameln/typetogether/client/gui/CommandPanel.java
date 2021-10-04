dfe26cc7069d2b4b332f119cabbd3ecbbe109094
leo25101 <leoniemersmann@web.de>
2021-10-04 16:15:27 +0200

    username and documentname korrektur

--- client/src/main/java/de/hswhameln/typetogether/client/gui/CommandPanel.java
+++ client/src/main/java/de/hswhameln/typetogether/client/gui/CommandPanel.java
@@ -7,9 +7,13 @@ import java.awt.Dimension;
 import java.awt.Color;
 import java.awt.event.ActionEvent;
 import java.awt.event.KeyEvent;
+import java.beans.PropertyChangeEvent;
 
+import de.hswhameln.typetogether.client.businesslogic.ClientUser;
+import de.hswhameln.typetogether.client.runtime.PropertyChangeManager;
 import de.hswhameln.typetogether.client.runtime.SessionStorage;
 import de.hswhameln.typetogether.client.runtime.commands.CommandInvoker;
+import de.hswhameln.typetogether.networking.api.Document;
 
 
 public class CommandPanel extends AbstractPanel {
@@ -17,15 +21,20 @@ public class CommandPanel extends AbstractPanel {
     private JButton undo;
     private JButton redo;
     private JLabel username;
+    private ClientUser user;
+    
     private JLabel documentname;
+    private Document sharedDocument;
     private JMenuBar menubar;
     private JPanel editorPanel;
     private CommandInvoker invoker;
+    private final PropertyChangeManager propertyChangeManager;
 
    public CommandPanel (MainWindow window, SessionStorage sessionStorage) {
         super(window, sessionStorage);
         this.invoker = sessionStorage.getCommandInvoker();
       
+       
         this.setLayout(new BorderLayout());
         this.setSize(ViewProperties.DEFAULT_WIDTH, ViewProperties.DEFAULT_HEIGHT);
         this.setPreferredSize(new Dimension(ViewProperties.DEFAULT_WIDTH, ViewProperties.DEFAULT_HEIGHT));
@@ -45,7 +54,16 @@ public class CommandPanel extends AbstractPanel {
 
         undo.addActionListener(a-> this.invoker.undo());
         redo.addActionListener(a-> this.invoker.redo());
-     username = new JLabel(sessionStorage.getCurrentUser().getName());
+        this.setUser(sessionStorage.getCurrentUser());
+        this.propertyChangeManager = new PropertyChangeManager();
+        sessionStorage.addPropertyChangeListener(this.propertyChangeManager);
+
+        this.propertyChangeManager.onPropertyChange(SessionStorage.CURRENT_USER, this::currentUserChanged);
+        this.propertyChangeManager.onPropertyChange(SessionStorage.CURRENT_SHARED_DOCUMENT, this::currentSharedDocumentChanged);
+
+
+        username = new JLabel();
+     
      documentname = new JLabel(sessionStorage.getCurrentSharedDocument().getFuncId());
 
         this.menubar.add(undo);
@@ -65,4 +83,26 @@ public class CommandPanel extends AbstractPanel {
     public void changeClickableRedo(boolean enabled) {
         this.redo.setEnabled(enabled);
     }
+    private void currentSharedDocumentChanged(PropertyChangeEvent propertyChangeEvent) {
+     this.setDocumentname ( (Document) propertyChangeEvent.getNewValue());
+ }
+ private void setDocumentname(Document newValue) {
+      this.sharedDocument = newValue;
+      this.documentname.setText(this.sharedDocument.getFuncId());
+}
+
+private void currentUserChanged(PropertyChangeEvent propertyChangeEvent) {
+     setUser((ClientUser) propertyChangeEvent.getNewValue());
+ }
+
+ private void setUser(ClientUser newUser) {
+     if (this.user != null) {
+         this.user.removePropertyChangeListener(this.propertyChangeManager);
+     }
+     this.user = newUser;
+     if (this.user != null) {
+         this.user.addPropertyChangeListener(this.propertyChangeManager);
+         this.username.setText(this.user.getName());
+     }
+ }
 }
