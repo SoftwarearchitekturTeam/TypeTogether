186ec878a2df42163c2d4dd5d365890597626ac1
Lars Kecker <lars.kecker2009@gmail.com>
2021-10-10 00:39:00 +0200

    fixed logger format

--- client.ps1
+++ client.ps1
@@ -3,4 +3,4 @@ param(
     [string]$port = '12557',
     [string]$loglevel = 'FINEST'
 )
-& java "-Durl=$url" "-Dport=$port" "-Dloglevel=$loglevel" -jar .\client\target\client-0.0.1-SNAPSHOT.jar
+& java '-Djava.util.logging.SimpleFormatter.format=%1$tY-%1$tm-%1$td %1$tH:%1$tM:%1$tS %4$-6s %2$-95s %5$s%6$s%n' "-Durl=$url" "-Dport=$port" "-Dloglevel=$loglevel" -jar .\client\target\client-0.0.1-SNAPSHOT.jar
--- server.ps1
+++ server.ps1
@@ -3,4 +3,4 @@ param(
     [string]$loglevel = 'FINEST'
 )
 & mvn clean install -DskipTests
-& java "-Dport=$port" "-Dloglevel=$loglevel" -jar .\server\target\server-0.0.1-SNAPSHOT.jar
+& java '-Djava.util.logging.SimpleFormatter.format=%1$tY-%1$tm-%1$td %1$tH:%1$tM:%1$tS %4$-6s %2$-95s %5$s%6$s%n' "-Dport=$port" "-Dloglevel=$loglevel" -jar .\server\target\server-0.0.1-SNAPSHOT.jar
